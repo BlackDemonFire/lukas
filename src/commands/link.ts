fcbf9db18b563ca6c3391a4bb102785fbb125a99
BlackDemonFire <bdf.sop@gmail.com>
2021-05-31 21:22:11 +0200

    Updated discord.js Added Link command

---
+++ src/commands/link.ts
@@ -0,0 +1,23 @@
+import { Bot } from "bot";
+import { Message, MessageEmbed, Permissions } from "discord.js";
+import { Command } from "../modules/command";
+
+export default class Link extends Command {
+    constructor(client: Bot) {
+        super(client);
+    }
+    async run(client: Bot, message: Message, args: string[], language: language) {
+        const embed: MessageEmbed = new MessageEmbed()
+            .setTitle("Links")
+            .addField("Invite", client.generateInvite({ additionalScopes: ["applications.commands"], permissions: Permissions.ALL }))
+            .addField("Github", "https://github.com/BlackDemonFire/lukas.git")
+            .setColor(0xaa7777);
+        message.channel.send(embed);
+    }
+    help = {
+        show: true,
+        name: "link",
+        usage: `${this.prefix}link`,
+        category: "Utility"
+    }
+}
