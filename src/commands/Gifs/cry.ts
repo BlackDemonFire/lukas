d2464c5d73928d92ae3c92e16fb3bcf2ad6610a5
Commentator2.0 <46420447+KommentatorForAll@users.noreply.github.com>
2023-07-15 22:44:51 +0200

    removed all code from all of the gif commands as it was just copies of each other. (#393)
    added SingleUserGifCommand and MultiUserGifCommand Classes to command.ts which contain the functionality originally in the command files

--- src/commands/Gifs/cry.ts
+++ src/commands/Gifs/cry.ts
@@ -1,35 +1,13 @@
-import type { ColorResolvable } from "discord.js";
-import { EmbedBuilder, Message } from "discord.js";
-import type { ILanguage as lang } from "src/types";
 import { Bot } from "../../bot.js";
-import { GifCommand } from "../../modules/command.js";
-export default class Cry extends GifCommand {
+import { SingleUserGifCommand } from "../../modules/command.js";
+
+export default class Cry extends SingleUserGifCommand {
   constructor(client: Bot, category: string, name: string) {
     super(client, category, name);
   }
+
   help = {
     show: true,
     usage: `${this.prefix}cry`,
   };
-  async run(client: Bot, message: Message, _args: string[], language: lang) {
-    const gif: string = await client.db.getgif(
-      "cry",
-      await client.db.getgiftype(message.author),
-    );
-    let userA: string = await client.db.getname(message.author);
-    const color: ColorResolvable = await client.db.getcolor(message.author);
-    if (userA == "")
-      userA = message.guild
-        ? message.member!.displayName
-        : message.author.username;
-    const responseString: string = (
-      await client.random.choice(language.command.cry.singleUser)
-    ).replace(/{a}/g, userA);
-    const embed = new EmbedBuilder()
-      .setImage(gif)
-      .setAuthor({ name: "cry" })
-      .setDescription(responseString)
-      .setColor(color);
-    message.channel.send({ embeds: [embed] });
-  }
 }
